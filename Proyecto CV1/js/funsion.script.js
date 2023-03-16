7de2e66a48bcdde925b1d9c12d51301c0c52dea2
lorenamoon <126127110+lorenamoon@users.noreply.github.com>
2023-03-16 19:04:25 -0300

    initial.commit

---
+++ Proyecto CV1/js/funsion.script.js
@@ -0,0 +1,53 @@
+document.getElementById('boton1').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("demo").innerHTML="NOMBRE: Brittany Day , ESTADO: casada /con 3hijos "
+}
+
+
+document.getElementById('boton2').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("email").innerHTML="Email:brittany.day@example.com /  Facebook: BRITTANY DAY  / Instagram: BRITTANY DAY 001"  ; 
+}
+
+document.getElementById('boton3').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("date").innerHTML= "9/8/1987  EDAD:35años";
+}
+
+
+document.getElementById('boton4').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("location").innerHTML="6071 Country Club Rd";
+}
+
+document.getElementById('boton5').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("phone").innerHTML=" (256) 909-0890";
+
+}
+
+document.getElementById('boton6').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("fa-graduation-cap").innerHTML="Estudios Primarios: Colegio “Ricardo Rojas/ Estudios Secundarios: Colegio I.P.E.M N° 21 Alfonsina Storni / Titulo: Ambiente y Salud /Estudios Universitarios: 1º AÑO Licenciatura en Diseño Gráfico facultad siglo 21/  CURSOS: (Preceptora) Instituto Nuestra Madre de la Merced/ (programacion) academy.profesional.ticmas.io , Diseño Grafico Blas Pascal,  ";
+
+}
+
+
+
+
+
+document.getElementById('boton7').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("fa-solid fa-briefcase").innerHTML="COLL CENTER tarea:(recpcionista atencion al cliente ,carga de datos, supervisora del personal,),Playa Maipuparking(encargada de turno, cajera, supervisora del personal),changoubera (asistente de marketing,asistente de virtual)";
+
+}
+
+document.getElementById('boton8').onclick = function(){
+    console.log("capturamos el elemento click");
+    document.getElementById("fa-solid fa-square-pluse").innerHTML="CONOCIMIENTO: Manejo PC,carga denter, manejo de paginas (facebook,instegram,whatsapp business,sitio web,) manejo de programas Word,paint,Excel, Photoshop,CorelDRAW,Canva, Visual studio codeHTM,CSS,JAVASCRIPT,  ";
+   
+
+    }
+
+  
+    console.log ("Hola mundo, !Hola profe Daniel!")
